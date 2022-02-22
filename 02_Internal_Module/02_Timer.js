06ee89b6ff74a0c945de09b9743f827d18a50146
YataNox <rladnwls0523@naver.com>
2022-02-22 16:38:57 +0900

    Timer.js Create

---
+++ 02_Internal_Module/02_Timer.js
@@ -0,0 +1,15 @@
+// 02_Timer.js
+
+// 지정된 시간 후에 한 번 실행
+const timeout = setTimeout(() => {
+   console.log('1.5초 후 실행'); 
+}, 1500);
+
+// 지정된 시간마다 반복 실행
+const interval = setInterval(()=>{
+    console.log('1초마다 실행');
+}, 1000);
+
+// 타이머 종료
+clearTimeout(timeout); // 아직 지정된 시간이 지나지 않았다면 실행전 종료
+clearInterval(interval); // 반복실행 종료
