ff3d4ad9a1628a03a63701d3eef73a32cab5b5f4
YataNox <rladnwls0523@naver.com>
2022-02-22 22:15:17 +0900

    code, coment Write

--- 03_HttpServer/04_Server04.js
+++ 03_HttpServer/04_Server04.js
@@ -4,7 +4,7 @@ const http = require('http');
 const fs = require('fs').promises;
 http.createServer(async (req, res)=>{
     try{
-    const date = await fs.readFile('./04_Server.html');
+    const data = await fs.readFile('./04_Server.html');
     res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
     res.end(data);
     }catch(err){
@@ -15,3 +15,9 @@ http.createServer(async (req, res)=>{
 }).listen(8081, ()=>{
     console.log('8081번 포트에서 서버 대기 중입니다.');
 });
+
+// http 상태 코트
+// 2xx : 서버 전송 정상 완료.
+// 3xx : 리다이렉션(다른 페이지 이동)을 알리는 상태
+// 4xx : 요청 오류를 나타냅니다. 요청 자체에 오류가 있을 때 표시됩니다.
+// 5xx : 서버 오류 - 요청은 제대로 왔지만 서버에 오류가 생겼을 때 발생합니다.
