831be604de42c0c5b20910e8ddc377091caa07ff
YataNox <rladnwls0523@naver.com>
2022-02-24 21:08:52 +0900

    npm morgan install

--- 04_Express_Server/NPM04/app.js
+++ 04_Express_Server/NPM04/app.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const path = require('path');
+// 추가 설치 모듈 require---------------------------------
+// 각각의 요청과 응답에 대한 필요정보를 보기 위한 모듈
+const morgan = require('morgan');
+// 쿠키 사용을 http 서버때보다 간결하게 사용하기 위한 모듈
+// const cookieParser = require('cookie-parser');
+// 세션 사용을 http 서버때보다 간결하게 사용하기 위한 모듈
+// const session = require('express-session');
+// const bodyParser = require('body-parser');
+//------------------------------------------------------------ 
+
 const app = express();
+
+app.set('port', process.env.PORT || 8090);
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(app.get('port'), ()=>{
+    console.log(app.get('port'), '번 포트에서 대기 중');
 });
