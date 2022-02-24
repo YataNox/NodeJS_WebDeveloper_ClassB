9f5cd35d41ada473bb5d9d6cef27bbd04569a66b
YataNox <rladnwls0523@naver.com>
2022-02-24 21:12:20 +0900

    npm cookieparser expresssession bodyparser install

--- 04_Express_Server/NPM04/app.js
+++ 04_Express_Server/NPM04/app.js
@@ -4,10 +4,11 @@ const path = require('path');
 // 각각의 요청과 응답에 대한 필요정보를 보기 위한 모듈
 const morgan = require('morgan');
 // 쿠키 사용을 http 서버때보다 간결하게 사용하기 위한 모듈
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 // 세션 사용을 http 서버때보다 간결하게 사용하기 위한 모듈
-// const session = require('express-session');
-// const bodyParser = require('body-parser');
+const session = require('express-session');
+// 요청의 본분을 해석 및 구분하는 모듈
+const bodyParser = require('body-parser');
 //------------------------------------------------------------ 
 
 const app = express();
