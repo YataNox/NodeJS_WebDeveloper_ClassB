b5a231e93b4c7e2a4aaf76d3b2002b975944bafc
YataNox <rladnwls0523@naver.com>
2022-02-28 16:55:22 +0900

    js. html rewrite

--- 04_Express_Server/NPM06/app.js
+++ 04_Express_Server/NPM06/app.js
@@ -1,43 +1,57 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookieParser');
+
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
+app.set('port', process.env.PORT || 3000);  
+
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(session({
+    resave:false,
+    saveUninitialized:false,
+    secret:"changdae",  
+}));  
 
-app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res)=>{
-    if(req.cookie.id){
-        res.send(`${req.cookie.id}님 반갑습니다.` + '<br><a href="/logout">로그아웃</a>');   
+app.get('/' , (req, res)=>{
+    if(req.cookies.id){ 
+        res.send(`${req.cookies.id} 님 반갑습니다` + '<br><a href="/logout">로그아웃</a>');
     }else{
         res.sendFile(path.join(__dirname, '/login.html'));
     }
 });
 
-app.post('/login', (req, res)=>{
+app.post('/login' , (req, res)=>{
     const id = req.body.id;
-    const pwd = req.body.pwd;
+    const pw = req.body.pw;
     const expires = new Date();
     expires.setMinutes(expires.getMinutes() + 1);
+    console.log(id, pw);
 
-    if(id == 'scott' && pw=='tiger'){
-        res.cookie('id', id,{
-            expires : expires,
-            httpOnly : true,
-            path : '/'
-        });
-        return res.json({msg:'ok'}); // json 데이터를 갖고 호출 위치로 되돌아 갑니다.
-        // send는 화면전환이 발생하지만 json은 화면 전환없이 리턴이 가능합니다.
+    if(id=='scott' && pw=='tiger'){    
+        res.cookie( 'id' , id , {expires : expires, httpOnly : true, path : '/' }); 
+        
+        return res.json({msg:'ok'}); // json 데이터를 갖고 호출위치로 되돌아갑니다.
+        // send는 화면 전환이 발생하지만, json은 화면 전환없이 리턴이 가능합니다.
     }else{
         
     }
+    
 });
 
-app.listen(app.get('port'), ()=>{
-    console.log(app.get('port'), '번 포트에서 대기중입니다.');
+app.get('/logout', (req, res) => {
+    // id, pw 쿠키를 지우고, / 로 리다이렉트
+    res.clearCookie( 'id' , req.cookies.name , {httpOnly:true, path:'/'});
+    res.clearCookie( 'pw' , req.cookies.name , {httpOnly:true, path:'/'});
+    res.redirect('/');
+});
+
+app.listen( app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기중입니다');
 });
