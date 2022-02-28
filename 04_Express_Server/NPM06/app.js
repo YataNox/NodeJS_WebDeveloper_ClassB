f5e39f63b51306e15b5dcd823f63d7441c2e6aa9
YataNox <rladnwls0523@naver.com>
2022-02-28 16:33:01 +0900

    morgan, parser~.... set

--- 04_Express_Server/NPM06/app.js
+++ 04_Express_Server/NPM06/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookieParser');
 const morgan = require('morgan');
-const { appendFileSync } = require('fs');
+const app = require('fs');
 
 const app = express();
 app.use(morgan('dev'));
@@ -11,6 +11,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/login.html'));
+});
 
 app.listen(app.get('port'), ()=>{
     console.log(app.get('port'), '번 포트에서 대기중입니다.');
