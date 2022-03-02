1bf327ee6a94fb53cff79b89dca6fe4182380956
YataNox <rladnwls0523@naver.com>
2022-03-02 17:28:47 +0900

    nodemon reset & pug file create

--- 04_Express_Server/NPM12/app.js
+++ 04_Express_Server/NPM12/app.js
@@ -4,10 +4,18 @@ const path = require('path');
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
+
+app.set('views', path.join(__dirname, 'views')); // 대상 퍼그 파일의 경로 설정
+app.set('view engine', 'put'); // 템플릿 엔진에 퍼그 설정
+
 app.use(morgan('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/login.html'));
+});
 
 app.listen( app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기중입니다');
