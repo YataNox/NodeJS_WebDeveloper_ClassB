deeae6a281879df6294ba583f7b6baf558cb46f9
YataNox <rladnwls0523@naver.com>
2022-03-02 21:09:10 +0900

    npm sequelize setting

--- 04_Express_Server/NPM13/app.js
+++ 04_Express_Server/NPM13/app.js
@@ -20,6 +20,11 @@ app.get('/', (req, res)=>{
     res.render('index', {title : 'Express'});
 })
 
+app.get('/include', (req, res)=>{
+    res.render('main', {title : 'Express'});
+})
+
+
 app.listen(app.get('port'), ()=>{
     console.log(app.get('port'), '번 포트에서 대기 중');
 });
