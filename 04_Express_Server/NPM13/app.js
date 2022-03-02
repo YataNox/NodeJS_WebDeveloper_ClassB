2d5949f85185b553b607bc6b4f844e6a7263257e
YataNox <rladnwls0523@naver.com>
2022-03-02 20:28:06 +0900

    nunjucks var~if write

---
+++ 04_Express_Server/NPM13/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const nunjucks = require('nunjucks');
+
+const app = express();
+app.set('port', process.env.POST || 3000);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+
+app.set('view engine', 'html');
+
+nunjucks.configure('views', {
+    express : app,
+    watch : true,
+})
+
+app.get('/', (req, res)=>{
+    res.render('index', {title : 'Express'});
+})
+
+app.listen(app.get('port'), ()=>{
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
