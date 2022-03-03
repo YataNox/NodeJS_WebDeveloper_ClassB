a046d4597a6bae3188f2baa95fcdc5dc9a28889a
YataNox <rladnwls0523@naver.com>
2022-03-03 16:50:44 +0900

    DBsetting & router files Create

--- 04_Express_Server/NPM14/app.js
+++ 04_Express_Server/NPM14/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const nunjucks = require('nunjucks');
 
 const app = express();
 app.set('port', process.env.POST || 3000);
@@ -8,7 +9,21 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 
-// const {sequelize} = require('/.models');
+// numjucks 사용환경 구성
+app.set('view engine', 'html');
+nunjucks.configure('views', {express : app, watch :true});
+
+// config.json의 내부 정보로 연결하기 윈한 db객체를 require합니다.
+const {sequelize} = require('./models');
+
+// 데이터 베이스 연결
+// 모델 제작 후 데이터베이스 연결시 해당 모델에 매핑되는 테이블이 없으면 새로 테이블을 만들라는 옵션.
+// force값이 true이면 기존 테이블도 지우고 강제로 만드렉 되니 주의하세요.
+sequelize.sync({force:false}).then(()=>{
+    console.log('데이터베이스 연결 성공');
+}).catch((err)=>{
+    console.error(err);
+});
 
 app.use((req, res, next) =>{
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
--- 04_Express_Server/NPM14/models/index.js
+++ 04_Express_Server/NPM14/models/index.js
@@ -1,5 +1,9 @@
 const Sequelize = require('sequelize');
 
+// 만들어놓은 테이블을 위한 모듈을 이곳에 require 합니다.
+const User = require('./user');
+const Comment = require('./comment');
+
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
@@ -7,7 +11,20 @@ const db = {};
 let sequelize;
 sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelize; // node.js에 접속하기 위한 접속객체를 db에 담습니다.
+db.Sequelize = Sequelize; // require한 순수 Sequelize모듈을 db에 담습니다.
+
+// require한 user모델과 comment모델도 db에 담습니다.
+db.User = User;
+db.Comment = Comment;
+
+// 모델 객체를 초기화하는 함수와 관계 형성 함수를 실행합니다.
+User.init(sequelize);
+Comment.init(sequelize);
+User.associate(db);
+Comment.associate(db);
+
+// 여기까지의 코드가 테이블이 생성되는 내용을 구성하는 코드입니다.
+// 밑에 코드처럼 db가 exports 되고, app.js에 require되면, require된 db에서 sequelize를 꺼내어서 sync함수를 실행하게 되고 이때 테이블도 생성합니다.
 
 module.exports = db;
