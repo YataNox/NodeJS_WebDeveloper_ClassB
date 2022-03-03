6fe03d734eca69189d0a3c9e0a84f65e431478e4
YataNox <rladnwls0523@naver.com>
2022-03-03 17:10:26 +0900

    index.get/ write

--- 04_Express_Server/NPM14/routes/index.js
+++ 04_Express_Server/NPM14/routes/index.js
@@ -3,3 +3,7 @@ const User = require('../models/user');
 const Comment = require('../models/comment');
 
 const router = express.Router();
+
+router.get('/', (req, res)=>{
+    res.render('sequelize', {}); // 최초 서버 실행 시 첫 페이지로 sequelize.html로 응답
+})
