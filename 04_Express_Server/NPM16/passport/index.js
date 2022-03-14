0694d3ba0f9ad39a5afb29b0e721d7f9d6ee9cde
YataNox <rladnwls0523@naver.com>
2022-03-14 22:22:41 +0900

    kakaologin (~ing)

--- 04_Express_Server/NPM16/passport/index.js
+++ 04_Express_Server/NPM16/passport/index.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const local = require('./localStrategy');
+const kakao = require('./kakaoStrategy');
 const User = require('../models/user');
 
 module.exports = () => {
@@ -17,4 +18,5 @@ module.exports = () => {
         .catch(err => done(err));
     });
     local();
+    kakao();
 };
--- 04_Express_Server/NPM16/routes/auth.js
+++ 04_Express_Server/NPM16/routes/auth.js
@@ -1,32 +1,30 @@
 const express = require('express');
 const User = require('../models/user');
-const bcrypt = require('bcrypt'); // 해시화
+const bcrypt = require('bcrypt');   // 해시, 암호화를 위한 모듈
 const passport = require('passport');
 
-const router = express.Router();
+const router =express.Router();
 
-// 일반 회원가입
+// 회원가입
 router.post('/join', async (req, res, next)=>{
-    const { email, nick, password } = req.body;
+    const {email, nick, password} = req.body;
     try{
-        const exUser = await User.findOne({ where : {email}}); // 이메일로 회원 검색
+        const exUser = await User.findOne({where:{email}}); // 이메일로 회원 검색
         if(exUser){ // 이미 해당 이메일 회원이 있다면
-            return res.redirect('/join?error=exist');
+            return res.redirect('/join?error=exist'); // 이메일 중복
         }
-        const hash = await bcrypt.hash(password, 12); // 12는 해시화를 하기위한 복잡도 인수. 숫자가 클수록 해시암호화가 복잡해지고 복구시간도 오래 걸립니다. 12가 약 1초 정도 시간의 실행을 해줍니다.
+        const hash = await bcrypt.hash(password, 12); // 12 : 해쉬화를 하기 위한 복잡도 인수. 숫자가 클수록 해시화 암호화가 복잡해지고 복구시간도 오래걸림. 12가 약 1초정도 시간의 실행을 해줍니다
         await User.create({
             email,
             nick,
             password:hash,
         }); // 이메일, 닉네임, 패스워드로 회원 추가
-        return res.redirect('/');
+        return res.redirect('/'); // 메인페이지로
     }catch(err){
         console.error(err);
-        next();
     }
 });
 
-// npm i passport passport-local passport-kakao bcrypt
 router.post('/login', (req, res, next)=>{
     // passport 모듈로 로그인을 구현합니다. 회원가입할때 입력한 패스워드도 해시화 (암호화)해서 저장하는걸로 전환됩니다.
     passport.authenticate('local', (authError, user, info)=>{
@@ -50,6 +48,12 @@ router.post('/login', (req, res, next)=>{
     })(req, res, next); // 미들웨어 내의 미들웨어이는 (req, res, next)를 붙입니다.
 });
 
+router.get('/kakao', passport.authenticate('kakao')); // 스트레티지를 통해 카카오에 한번 갔다가 콜백받아서 돌아오고
 
+router.get('/kakao/callback', passport.authenticate('kakao', { // 그다음은 요부분 실행
+    failureRedirect: '/',
+}),(req, res)=>{
+    res.redirect('/');
+});
 
 module.exports = router;
